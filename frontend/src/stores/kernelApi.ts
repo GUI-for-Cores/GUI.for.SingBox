15faf739658e887182a41f67274c82873d542eaf
gui.for.singbox <gui.for.singbox@dev.com>
2024-01-08 00:53:54 +0800

    update  state restore

--- frontend/src/stores/kernelApi.ts
+++ frontend/src/stores/kernelApi.ts
@@ -3,7 +3,12 @@ import { defineStore } from 'pinia'
 
 import { generateConfigFile, ignoredError } from '@/utils'
 import type { KernelApiConfig, Proxy } from '@/api/kernel.schema'
-import { KernelWorkDirectory, getKernelFileName } from '@/constant'
+import {
+  KernelWorkDirectory,
+  getKernelFileName,
+  KernelConfigFilePath,
+  StackOptions
+} from '@/constant'
 import {
   type ProfileType,
   useAppSettingsStore,
@@ -12,7 +17,14 @@ import {
   useEnvStore
 } from '@/stores'
 import { getProxies, getProviders } from '@/api/kernel'
-import { EventsOn, KernelRunning, KillProcess, StartKernel, GetInterfaces } from '@/utils/bridge'
+import {
+  EventsOn,
+  KernelRunning,
+  KillProcess,
+  StartKernel,
+  GetInterfaces,
+  Readfile
+} from '@/utils/bridge'
 import { deepClone } from '@/utils/others'
 
 export const useKernelApiStore = defineStore('kernelApi', () => {
@@ -56,6 +68,24 @@ export const useKernelApiStore = defineStore('kernelApi', () => {
       const result = deepClone(profilesStore.getProfileById(profileID)) as ProfileType
       const interfaces = await GetInterfaces()
       result.tunConfig.enable = interfaces.some((f) => f === result.tunConfig.interface_name)
+
+      if (config.value.mode) {
+        result.generalConfig.mode = config.value.mode
+        result.advancedConfig.port = config.value.port
+        result.generalConfig['mixed-port'] = config.value['mixed-port']
+        result.advancedConfig['socks-port'] = config.value['socks-port']
+        result.generalConfig['log-level'] = config.value['log-level']
+        result.generalConfig['allow-lan'] = config.value['allow-lan']
+        result.generalConfig['interface-name'] = config.value['interface-name']
+        result.dnsConfig.fakeip = config.value.fakeip
+        if (config.value.tun.device) {
+          result.tunConfig.enable = config.value.tun.enable
+          result.tunConfig.stack = config.value.tun.stack
+          result.tunConfig['auto-route'] = config.value.tun['auto-route']
+          result.tunConfig.interface_name = config.value.tun.device
+        }
+      }
+
       currentProfile.value = result
     }
   }
@@ -118,6 +148,47 @@ export const useKernelApiStore = defineStore('kernelApi', () => {
     providers.value = a
     proxies.value = b
   }
+
+  const initConfig = async () => {
+    const content = (await ignoredError(Readfile, KernelConfigFilePath)) || '{}'
+    if (!content) return
+    try {
+      const latestConfig: Record<string, any> = JSON.parse(content)
+      config.value.port = 0
+      config.value['mixed-port'] = 0
+      config.value['socks-port'] = 0
+      let inbound_tun: any = {}
+      latestConfig.inbounds.forEach((inbound: any) => {
+        if (inbound.type == 'tun') inbound_tun = inbound
+        else if (inbound.type == 'mixed') config.value['mixed-port'] = inbound.listen_port
+        else if (inbound.type == 'http') config.value.port = inbound.listen_port
+        else if (inbound.type == 'socks') config.value['socks-port'] = inbound.listen_port
+        if (inbound.listen_port) {
+          config.value['allow-lan'] = inbound.listen === '::'
+        }
+      })
+      config.value.mode = latestConfig.experimental.clash_api.default_mode
+      config.value['log-level'] = latestConfig.log.level
+      config.value.fakeip = latestConfig.dns.fakeip.enabled
+      if (latestConfig.route.auto_detect_interface) config.value['interface-name'] = 'Auto'
+      else if (latestConfig.route.default_interface)
+        config.value['interface-name'] = latestConfig.route.default_interface
+      if (inbound_tun) {
+        const stack = StackOptions.filter((s) => s.value.toLowerCase() === inbound_tun.stack)
+        const stackName = stack ? stack[0].value : inbound_tun.stack
+        config.value.tun = {
+          enable: true,
+          stack: stackName,
+          'auto-route': inbound_tun.auto_route,
+          device: inbound_tun.interface_name
+        }
+      }
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+  initConfig()
 
   /* Bridge API */
   const loading = ref(false)
