747bf3467372851a46723df45d5178437176a977
Ayideyia <150233177+Ayideyia@users.noreply.github.com>
2025-01-21 22:36:33 +0800

    chore: update Vite configuration for base path and formatting

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,21 +5,22 @@ import Components from 'unplugin-vue-components/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [
     vue(),
     Components({
       types: [],
       dts: 'src/components/components.d.ts',
-      globs: ['src/components/*/index.vue']
-    })
+      globs: ['src/components/*/index.vue'],
+    }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
-      '@wails': fileURLToPath(new URL('./wailsjs', import.meta.url))
-    }
+      '@wails': fileURLToPath(new URL('./wailsjs', import.meta.url)),
+    },
   },
   build: {
-    chunkSizeWarningLimit: 2048 // 2MB
-  }
+    chunkSizeWarningLimit: 2048, // 2MB
+  },
 })
