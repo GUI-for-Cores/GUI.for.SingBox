eddc804a47e3f7ad2ee0f8ee9cd5e8f9923f9ff4
Ayideyia <150233177+Ayideyia@users.noreply.github.com>
2025-04-04 01:12:52 +0800

    refactor: streamline component registration logic

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -4,15 +4,16 @@ export { default as MainPage } from './MainPage.vue'
 export { default as TitleBar } from './TitleBar.vue'
 export { default as NavigationBar } from './NavigationBar.vue'
 
-type GlobFuncType = Record<string, { default: Component }>
-
-const Components: GlobFuncType = import.meta.glob('./*/index.vue', { eager: true })
+const Components = import.meta.glob<Component>('./*/index.vue', {
+  eager: true,
+  import: 'default',
+})
 
 export default {
   install: (app: App) => {
     for (const path in Components) {
       const name = path.split('/')[1]
-      app.component(name, Components[path].default)
+      app.component(name, Components[path])
     }
   },
 } as Plugin
