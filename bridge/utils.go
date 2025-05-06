75a088cacab4af7bd232af819733006df3b52a47
Ayideyia <150233177+Ayideyia@users.noreply.github.com>
2025-05-06 11:04:50 +0800

    perf: refactor GetTimeout and optimize GetHeader functions.

--- bridge/utils.go
+++ bridge/utils.go
@@ -32,15 +32,15 @@ func GetProxy(_proxy string) func(*http.Request) (*url.URL, error) {
 	return proxy
 }
 
-func GetTimeout(_timeout int) time.Duration {
-	if _timeout == 0 {
-		return time.Second * 15
+func GetTimeout(timeout int) time.Duration {
+	if timeout <= 0 {
+		return 15 * time.Second
 	}
-	return time.Second * time.Duration(_timeout)
+	return time.Duration(timeout) * time.Second
 }
 
 func GetHeader(headers map[string]string) http.Header {
-	header := make(http.Header)
+	header := make(http.Header, len(headers))
 	for key, value := range headers {
 		header.Set(key, value)
 	}
