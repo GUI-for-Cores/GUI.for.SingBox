f12db294fa5b338d306bcad0861599dcadfdd002
Ayideyia <150233177+Ayideyia@users.noreply.github.com>
2025-10-03 15:17:12 +0800

    feat(InputList): add unique IDs for items and support editing

--- frontend/src/components/InputList/index.vue
+++ frontend/src/components/InputList/index.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
-import { ref, useTemplateRef } from 'vue'
+import { ref, useTemplateRef, watch } from 'vue'
 
 import { DraggableOptions } from '@/constant/app'
+import { sampleID } from '@/utils'
 
 import Input from '@/components/Input/index.vue'
 
@@ -10,31 +11,75 @@ interface Props {
   autofocus?: boolean
 }
 
+interface Item {
+  value: string
+  id: string
+}
+
 withDefaults(defineProps<Props>(), { autofocus: true })
 
 const list = defineModel<string[]>({ default: [] })
+const innerList = ref<Item[]>(list.value.map((v, i) => ({ value: v, id: i.toString() })))
 
-const value = ref('')
+const editItem = ref<Item>()
+const inputVal = ref('')
 const inputRef = useTemplateRef<typeof Input>('inputRef')
 
 const handleAdd = () => {
-  if (!value.value) return
-  list.value.push(value.value)
-  value.value = ''
+  const item = editItem.value
+  editItem.value = undefined
+  if (!inputVal.value) return
+  if (item) {
+    item.value = inputVal.value
+  } else {
+    innerList.value.push({ value: inputVal.value, id: sampleID() })
+  }
+  inputVal.value = ''
   inputRef.value?.focus()
 }
 
-const handleDel = (i: number) => list.value.splice(i, 1)
+const handleEdit = (item: Item) => {
+  editItem.value = item
+  inputVal.value = editItem.value.value
+}
+
+const handleDel = (item: Item) => {
+  const idx = innerList.value.indexOf(item)
+  if (idx !== -1) {
+    innerList.value.splice(idx, 1)
+  }
+}
+
+watch(
+  innerList,
+  (v) => {
+    list.value = v.map((v) => v.value)
+  },
+  { deep: true },
+)
 </script>
 
 <template>
   <div class="gui-input-list inline-block rounded-4">
-    <div v-draggable="[list, DraggableOptions]" class="flex flex-col gap-2">
+    <div v-draggable="[innerList, DraggableOptions]" class="flex flex-col gap-2">
       <TransitionGroup name="list">
-        <Card v-for="(l, i) in list" :key="i">
-          <div class="flex items-center justify-between py-4 break-all">
-            {{ l }}
-            <Button @click="handleDel(i)" icon="close" :icon-size="10" type="text" />
+        <Card v-for="item in innerList" :key="item.id">
+          <div class="flex items-center gap-4 py-4 break-all">
+            <Button
+              @click="handleEdit(item)"
+              icon="edit"
+              :icon-size="12"
+              size="small"
+              type="text"
+            />
+            <span class="mr-auto">{{ item.value }}</span>
+            <Button
+              @click="handleDel(item)"
+              icon="close"
+              :icon-size="12"
+              size="small"
+              type="text"
+            />
           </div>
         </Card>
       </TransitionGroup>
@@ -42,7 +87,7 @@ const handleDel = (i: number) => list.value.splice(i, 1)
 
     <Input
       ref="inputRef"
-      v-model="value"
+      v-model="inputVal"
       :placeholder="placeholder"
       @keydown.enter="handleAdd"
       type="text"
@@ -51,7 +96,7 @@ const handleDel = (i: number) => list.value.splice(i, 1)
       class="mt-4 w-full"
     >
       <template #suffix>
-        <Button @click="handleAdd" icon="add" size="small" type="primary" />
+        <Button @click="handleAdd" :icon="editItem ? 'edit' : 'add'" size="small" type="primary" />
       </template>
     </Input>
   </div>
