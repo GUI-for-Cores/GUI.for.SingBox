1b4ba20156d3cca75b4a38b2495cb6d33e5ca503
Ayideyia <150233177+Ayideyia@users.noreply.github.com>
2026-01-25 20:57:16 +0800

    fix: add size to Icon SVG

--- frontend/src/components/Icon/index.vue
+++ frontend/src/components/Icon/index.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { computed } from 'vue'
+
 import { icons, type IconName } from './icons'
 
 interface Props {
@@ -7,24 +9,21 @@ interface Props {
   color?: string
 }
 
-withDefaults(defineProps<Props>(), { size: 16, color: 'var(--color)' })
+const props = withDefaults(defineProps<Props>(), { size: 16, color: 'var(--color)' })
+
+const sizeWithUnit = computed(() => props.size + 'px')
 </script>
 
 <template>
   <div class="inline-flex">
-    <span
-      class="icon flex items-center justify-center"
-      :style="{
-        width: size + 'px',
-        height: size + 'px',
-      }"
-      v-html="icons[icon] || icons.error"
-    />
+    <span class="icon flex items-center justify-center" v-html="icons[icon] || icons.error" />
   </div>
 </template>
 
 <style>
 .icon svg {
+  width: v-bind(sizeWithUnit);
+  height: v-bind(sizeWithUnit);
   fill: v-bind(color);
 }
 </style>
