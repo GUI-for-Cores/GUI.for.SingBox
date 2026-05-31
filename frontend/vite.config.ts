01d4e14044b3d0290002802b61194664a05085a4
Ayideyia <150233177+Ayideyia@users.noreply.github.com>
2026-05-31 23:32:55 +0800

    fix: ignore wails generated files in vite watcher

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,16 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
   plugins: [vue()],
+  server: {
+    watch: {
+      ignored: ['**/node_modules/**', '**/.git/**', '**/dist/**', '**/src/bridge/wailsjs/**'],
+    },
+  },
   resolve: {
     extensions: ['.ts', '.js'],
     alias: {
