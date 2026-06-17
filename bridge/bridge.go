a3bd823039f98ceda46615f2a3c1422afccf5b18
Ayideyia <150233177+Ayideyia@users.noreply.github.com>
2026-06-18 01:55:15 +0800

    Release v1.25.2

--- bridge/bridge.go
+++ bridge/bridge.go
@@ -28,7 +28,7 @@ var Env = &EnvResult{
 	FromTaskSch:  false,
 	WebviewPath:  "",
 	AppName:      "",
-	AppVersion:   "v1.25.1",
+	AppVersion:   "v1.25.2",
 	BasePath:     "",
 	OS:           sysruntime.GOOS,
 	ARCH:         sysruntime.GOARCH,
