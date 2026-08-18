bc524891fc5ac6a6954bf6b2cb45e20c75b2566c
Ayideyia <150233177+Ayideyia@users.noreply.github.com>
2026-08-18 22:53:25 +0800

    fix: refresh subscription names in outbound sorting

--- frontend/src/views/ProfilesView/components/OutboundSort.vue
+++ frontend/src/views/ProfilesView/components/OutboundSort.vue
@@ -2,6 +2,7 @@
 import { useI18n } from 'vue-i18n'
 
 import { DraggableOptions } from '@/constant/app'
+import { useSubscribesStore } from '@/stores'
 
 interface Props {
   outbound: App.Outbound
@@ -10,6 +11,14 @@ interface Props {
 defineProps<Props>()
 
 const { t } = useI18n()
+const subscribesStore = useSubscribesStore()
+
+const getOutboundName = (item: App.Proxy) => {
+  if (item.type === 'Subscription') {
+    return subscribesStore.getSubscribeById(item.id)?.name || item.tag
+  }
+  return item.tag
+}
 </script>
 
 <template>
@@ -17,7 +26,7 @@ const { t } = useI18n()
   <Empty v-if="outbound.outbounds.length === 0" />
   <div v-draggable="[outbound.outbounds, DraggableOptions]">
     <Button v-for="item in outbound.outbounds" :key="item.id" type="link">
-      {{ item.tag }}
+      {{ getOutboundName(item) }}
     </Button>
   </div>
 </template>
